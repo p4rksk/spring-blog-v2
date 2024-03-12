84fff768af5817636d9516938203337d99ffc447
yeeun0118s <yeeun0118s@naver.com>
2024-03-12 16:02:55 +0900

    게시글 수정

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -16,7 +16,20 @@ public class BoardController {
 
     private final BoardNativeRepository boardNativeRepository;
 
-    @PostMapping("/board/{id}delete")
+    @PostMapping("/board/{id}/update")
+    public String update(@PathVariable Integer id, String title, String content, String username){
+        boardNativeRepository.updateById(id,title,content,username);
+        return "redirect:/board/"+id;
+    }
+
+    @GetMapping("/board/{id}/update-form")
+    public String updateForm(@PathVariable Integer id,HttpServletRequest request){
+        Board board = boardNativeRepository.findById(id);
+        request.setAttribute("board",board);
+        return "board/update-form";
+    }
+
+    @PostMapping("/board/{id}/delete")
     public String delete(@PathVariable Integer id){
         boardNativeRepository.deleteById(id);
         return "redirect:/";
--- src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
@@ -18,6 +18,26 @@ public class BoardNativeRepositoryTest {
     private BoardNativeRepository boardNativeRepository;
 
     @Test
+    public void updateById_test(){
+        //given(파라미터에 들어 갈것들)
+        int id = 1;
+        String title = "제목 수정 1";
+        String content = "내용 수정 1";
+        String username = "bori";
+
+        //when(내가 하려는 내용)
+        boardNativeRepository.updateById(id,title,content,username);
+
+
+        //then(검증 단계)
+        Board board = boardNativeRepository.findById(id);
+        assertThat(board.getUsername()).isEqualTo("bori");
+        assertThat(board.getTitle()).isEqualTo("제목 수정 1");
+        assertThat(board.getContent()).isEqualTo("내용 수정 1");
+    }
+
+
+    @Test
     public void deleteById_test(){
         //given
         int id = 1;
