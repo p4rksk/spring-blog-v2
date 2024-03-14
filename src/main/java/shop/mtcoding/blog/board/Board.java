aed223e4e7e105675015ce9566251c9bf52b63e1
yeeun0118s <yeeun0118s@naver.com>
2024-03-14 15:43:24 +0900

    게시글 쓰기 - 연관관계 persist 완료

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -1,39 +1,38 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import shop.mtcoding.blog.user.User;
-import shop.mtcoding.blog.util.MyDateUtil;
 
 import java.sql.Timestamp;
 
-@NoArgsConstructor
+@NoArgsConstructor //엔티티는 디폴트 생성자 무조건 있어야 함
 @Data
 @Table(name = "board_tb")
 @Entity
 public class Board {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String content;
 
-//    @JoinColumn(name = "user_id")
-    @ManyToOne(fetch = FetchType.LAZY)//연관 관계로 본다
-    private User user; //user_id  변수명(객체)_객체 필드 명(칼럼)
+    //@JoinColumn(name = "user_id")
+    @ManyToOne (fetch = FetchType.LAZY)
+    private User user; //db -> user_id
 
-    @CreationTimestamp//ps를 -> db로 갈때 날짜로 주입 됨
+    @CreationTimestamp // pc -> db (날짜가 주입됨)
     private Timestamp createdAt;
 
-    public String getTime(){
-        return MyDateUtil.timestampFormat(createdAt);
+    @Builder
+    public Board(Integer id, String title, String content, User user, Timestamp createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.createdAt = createdAt;
     }
-
-
-
-
 }
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.blog.user.User;
 
 import java.util.HashSet;
@@ -14,6 +15,12 @@ import java.util.Set;
 @Repository
 public class BoardRepository {
     private final EntityManager em;
+
+    @Transactional
+    public Board save(Board board){
+        em.persist(board);
+        return board;
+    }
 
 
     public List<Board> findAllV2() {
